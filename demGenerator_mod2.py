a408ce7e7600d0eeb858d614e7bf451438797891
RusheerPL <35576727+RusheerPL@users.noreply.github.com>
2020-02-02 13:44:11 +0100

    Add posibility to batch processing whole folder
    First step – only demGen_data is allowed. https://github.com/RusheerPL/demGenerator/issues/5

--- demGenerator_mod2.py
+++ demGenerator_mod2.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import platform
 from multiprocessing import Pool
 from pathlib import Path
@@ -7,7 +8,7 @@ import urllib.request
 import math
 import tqdm
 import ast
-    
+
 def openUrl(url):
     connerr = True
     while (connerr):
@@ -37,93 +38,107 @@ if __name__ == '__main__':
             config = open("demGenerator_config.txt", "r")
         except:
             country = input("Can't load the config file\nCountry code (PL/CZ): ")
+            div = int(input("Tiles count: "))
         else:
             print("Config file is loading...")
-            country = config.read()
-            country = country.split()[0]
+            cnf = config.read().split()
+            country, div = cnf[0], int(cnf[7])
             print("Country: %s" % country)
         while (country != "CZ" and country != "PL"):
             country = input("Re-enter the country code (PL/CZ): ")
-
-        part = int(input("Tile number: "))
-        while (not Path("demGen_data/xy_%d.txt" % part).exists()):
-            part = int(input("'xy_%d.txt' doesn't exist - re-enter the tile number: " % part))
-        data = open("demGen_data/xy_%d.txt" % part)
-        i = 0
-        we = []
-        while ("true"):
-            string = data.readline()
-            tab = string.split()
-            if (len(tab) != 0):
-                we.append([float(tab[0]), float(tab[1])])
-                i += 1
-            else:
-                break
-        data.close()
-        print("Coordinates were read from 'xy_%d.txt'" % part)
-        l = int(math.floor(math.sqrt(i)))
-        if (math.pow(l, 2) != i):
-            print("Incorrect count of input data! Is: %d. Should be: %d. Check the 'xy_%d.txt' file ontent and correct the mistakes." % (i, math.pow(l, 2), part))
-            del we
+            
+        tiles = []
+        alltiles = '0'
+        
+        while not alltiles.isalpha() or (alltiles.isalpha() and (alltiles.lower() != 'y' and alltiles.lower() != 'n')):
+            alltiles = input("Do you want to process all 'xy_*.txt' files from 'demGen_data' directory? (y/n): ")
+        if alltiles.lower() == 'n':
+            tile = int(input("Tile number: "))
+            while not Path("demGen_data/xy_%d.txt" % tile).exists():
+                tile = int(input("'xy_%d.txt' doesn't exist - re-enter the tile number: " % tile))
+            tiles.append(tile)
         else:
-            print("Tile size: (%d x %d) px" % ((l, l)))
-            hstr = []
-            wy = []
-            
-            print("Generating query URLs...")
-            rqs = []
-            if country == 'CZ':
-                jm = 37.0
-            else:
-                jm = 200.0
-            jmax = int(math.ceil(len(we) / jm))
-            with tqdm.tqdm(total=jmax) as pbar:
-                for j in range(jmax):
-                    if country == 'CZ':
-                        url = 'https://ags.cuzk.cz/arcgis2/rest/services/dmr5g/ImageServer/getSamples?geometry=%7Bpoints%3A%5B'
-                    elif country == 'PL':
-                        url = 'https://services.gugik.gov.pl/nmt/?request=GetHbyXY&xy='
-                    for k in range(int(jm) * j, int(jm) * (j + 1)):
-                        if (k >= len(we)):
-                            break
-                        if country == 'CZ':
-                            url += '%5B' + str(-we[k][0]) + '%2C' + str(-we[k][1]) + '%5D'
-                            if k < jm * (j + 1) - 1:
-                                url += '%2C'
-                        elif country == 'PL':
-                            url += str(we[k][1]) + '%20' + str(we[k][0]) + ','
-                    if country == 'CZ':
-                        url += '%5D%2CspatialReference%3A%7Bwkid%3A5514%7D%7D&geometryType=esriGeometryMultipoint&sampleDistance=&sampleCount=&mosaicRule=&pixelSize=&returnFirstValueOnly=false&f=pjson'
-                    rqs.append(url)
-                    pbar.update(1)
-            rqlist = open('demGen_data/requestsList.txt', 'w')
-            for j in range(len(rqs)):
-                rqlist.write(rqs[j] + '\n')
-            rqlist.close()
-            
-            # Elevation data downloading
-            if (country == "CZ"):
-                print("Downloading data from 'ags.cuzk.cz/arcgis2/rest/services/dmr5g/ImageServer'...\nThis operation may take some time.")
-                with Pool(64) as p:
-                    hstr = list(tqdm.tqdm(p.imap(getHeightCZ, rqs), total=len(rqs)))
-            elif (country == "PL"):
-                print("Downloading data from 'services.gugik.gov.pl/nmt'...\nThis operation may take some time.")
-                with Pool(64) as p:
-                    hstr = list(tqdm.tqdm(p.imap(openUrl, rqs), total=len(rqs))) # openUrl instead of getHeightPL
+            for i in range(div):
+                if Path("demGen_data/xy_%d.txt" % i).exists():
+                    tiles.append(i)
 
-            for j in range(len(hstr)):
-                h2str = hstr[j].split(',')
-                if len(h2str) < jm and j != len(hstr) - 1:
-                    print('Insufficient number of points – see query #%d' % (j + 1))
-                for k in range(len(h2str)):
-                    if k < jm:
-                        wy.append(h2str[k])
-            output = open("demGen_data/h_%d.txt" % part, "w")
-            for j in range(len(wy)):
-                output.write(str(wy[j]) + "\n") # dane uporządkowane
-            output.close()
-            del we, wy, hstr
-            print("Elevation data downloaded and saved in 'h_%d.txt'." % part)
+        for part in tiles:
+            data = open("demGen_data/xy_%d.txt" % part)
+            i = 0
+            we = []
+            while True:
+                string = data.readline()
+                tab = string.split()
+                if len(tab) != 0:
+                    we.append([float(tab[0]), float(tab[1])])
+                    i += 1
+                else:
+                    break
+            data.close()
+            print("\nCoordinates were read from 'xy_%d.txt'" % part)
+            l = int(math.floor(math.sqrt(i)))
+            if math.pow(l, 2) != i:
+                print("Incorrect count of input data! Is: %d. Should be: %d. Check the 'xy_%d.txt' file ontent and correct the mistakes." % (i, math.pow(l, 2), part))
+                del we
+            else:
+                print("Tile size: (%d x %d) px" % (l, l))
+                hstr = []
+                wy = []
+                
+                print("Generating query URLs...")
+                rqs = []
+                if country == 'CZ':
+                    jm = 37.0
+                else:
+                    jm = 200.0
+                jmax = int(math.ceil(len(we) / jm))
+                with tqdm.tqdm(total=jmax) as pbar:
+                    for j in range(jmax):
+                        if country == 'CZ':
+                            url = 'https://ags.cuzk.cz/arcgis2/rest/services/dmr5g/ImageServer/getSamples?geometry=%7Bpoints%3A%5B'
+                        elif country == 'PL':
+                            url = 'https://services.gugik.gov.pl/nmt/?request=GetHbyXY&xy='
+                        for k in range(int(jm) * j, int(jm) * (j + 1)):
+                            if k >= len(we):
+                                break
+                            if country == 'CZ':
+                                url += '%5B' + str(-we[k][0]) + '%2C' + str(-we[k][1]) + '%5D'
+                                if k < jm * (j + 1) - 1:
+                                    url += '%2C'
+                            elif country == 'PL':
+                                url += str(we[k][1]) + '%20' + str(we[k][0]) + ','
+                        if country == 'CZ':
+                            url += '%5D%2CspatialReference%3A%7Bwkid%3A5514%7D%7D&geometryType=esriGeometryMultipoint&sampleDistance=&sampleCount=&mosaicRule=&pixelSize=&returnFirstValueOnly=false&f=pjson'
+                        rqs.append(url)
+                        pbar.update(1)
+                rqlist = open('demGen_data/requestsList_%d.txt' % part, 'w')
+                for j in range(len(rqs)):
+                    rqlist.write(rqs[j] + '\n')
+                rqlist.close()
+
+                # Elevation data downloading
+                if country == "CZ":
+                    print("Downloading data from 'ags.cuzk.cz/arcgis2/rest/services/dmr5g/ImageServer'...\nThis operation may take some time.")
+                    with Pool(64) as p:
+                        hstr = list(tqdm.tqdm(p.imap(getHeightCZ, rqs), total=len(rqs)))
+                elif country == "PL":
+                    print("Downloading data from 'services.gugik.gov.pl/nmt'...\nThis operation may take some time.")
+                    with Pool(64) as p:
+                        hstr = list(tqdm.tqdm(p.imap(openUrl, rqs), total=len(rqs))) # openUrl instead of getHeightPL
+
+                for j in range(len(hstr)):
+                    h2str = hstr[j].split(',')
+                    if len(h2str) < jm and j != len(hstr) - 1:
+                        print('Insufficient number of points – see query #%d' % (j + 1))
+                    for k in range(len(h2str)):
+                        if k < jm: # warunek
+                            wy.append(h2str[k])
+                output = open("demGen_data/h_%d.txt" % part, "w")
+                for j in range(len(wy)):
+                    output.write(str(wy[j]) + "\n") # dane uporządkowane
+                output.close()
+                del we, wy, hstr
+                print("Elevation data downloaded and saved in 'h_%d.txt'." % part)
     else:
         print("This script has no support for Python %s.%s.%s. Run it in the Python 3.5 - 3.7 environment." % (ver[0], ver[1], ver[2]))
     w = input("Press ENTER to close...")
