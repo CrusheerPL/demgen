a1b8902f22c1af64298df34d2a76b0568b426bd0
CrusheerPL <35576727+CrusheerPL@users.noreply.github.com>
2020-09-11 23:18:09 +0200

    Updated mod2 for QGIS (SK only)
    - You can normally select the demGen's input data directory instead of typing its whole path
    - Added support for processing of more than 1 "xy" txt file

--- demGenerator_mod2_qgis.py
+++ demGenerator_mod2_qgis.py
@@ -1,32 +1,38 @@
-from PyQt5.QtWidgets import QInputDialog
-import math
+from qgis.PyQt.QtWidgets import QFileDialog
+import math, os
 
-k, v = QInputDialog().getText(None, "Input", "Enter the 'demGen_data' directory path:")
-
-if v and k != '':
+indir = QFileDialog.getExistingDirectory(iface.mainWindow(), "Select the demGen's input data directory")
+if indir != '':
     rlayer = iface.activeLayer()
-    data = open(k + '/xy_0.txt', 'r')
-    res = open(k + '/h_0.txt', 'w')
-    i = 0
-    we = []
-    while ("true"):
-        string = data.readline()
-        tab = string.split()
-        if (len(tab) != 0):
-            we.append([float(tab[0]), float(tab[1])])
-            i += 1
-        else:
-            break
-    data.close()
-    print('Coordinates were read.')
-    l = int(math.floor(math.sqrt(i)))
-    if (math.pow(l, 2) != i):
-        print("Incorrect count of input data! Is: %d. Should be: %d. Check the 'xy_0.txt' file ontent and correct the mistakes." % (i, math.pow(l, 2), part))
-        del we
-    else:
-        print("Tile size: (%d x %d) px\nElevation data reading in progress - this operation may take some time." % ((l, l)))
-        for i in range(len(we)):
-            ident = rlayer.dataProvider().identify(QgsPointXY(-we[i][0], -we[i][1]), QgsRaster.IdentifyFormatValue)
-            res.write('%f\n' % ident.results()[1])
-        print("Ready – elevation data saved in 'h_0.txt'.")
-    res.close()
+    if rlayer != None:
+        fns = []
+        for i in os.listdir(path = indir):
+            if 'xy_' in i: fns.append(i)
+        for fn in fns:
+            data = open('%s/%s' % (indir, fn), 'r')
+            res = open('%s/h_%s' % (indir, fn[3:]), 'w')
+            i = 0
+            we = []
+            while True:
+                string = data.readline()
+                tab = string.split()
+                if len(tab) != 0:
+                    we.append([float(tab[0]), float(tab[1])])
+                    i += 1
+                else: break
+            data.close()
+            print('%s: input data read successfully' % fn)
+            l = int(math.floor(math.sqrt(i)))
+            if math.pow(l, 2) != i:
+                print("Incorrect count of input data! Is: %d. Should be: %d. Check the '%s' file content and correct the mistakes." % (i, math.pow(l, 2), fn))
+                del we
+            else:
+                print("Tile size: (%d x %d) px\nElevation data reading..." % ((l, l)))
+                for i in range(len(we)):
+                    ident = rlayer.dataProvider().identify(QgsPointXY(-we[i][0], -we[i][1]), QgsRaster.IdentifyFormatValue)
+                    if ident.results()[1] != None:
+                        res.write('%f\n' % ident.results()[1])
+                    else:
+                        res.write('0\n')
+                print("Ready – elevation data saved to 'h_%s' file" % fn[3:])
+            res.close()
