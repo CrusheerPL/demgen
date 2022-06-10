aff6e785b0dc913ca24b8116e6423b3a4a51a37f
CrusheerPL <35576727+CrusheerPL@users.noreply.github.com>
2022-06-10 23:19:14 +0200

    Update mapGen.py & requirementsChecker.py
    mapGen.py:
    - added support for FS 22 mod-maps
    - added PL translation
    - added feature: automatic terrain textures convertion to DDS format (GIANTS Texture Tool for FS 22 textures, texconv.exe for FS 19 and older textures)
    - added feature: saving the full-size map overview & terrain shading images
    - library migration: urllib.request > requests
    
    requirementsChecker.py:
    - added feature: check if the Texconv and GIANTS Texture Tool are present in the demGen's directory

--- mapGen.py
+++ mapGen.py
@@ -1,35 +1,45 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
+import math, locale, requests, io, os, subprocess, tqdm
+from PIL import Image
+requests.urllib3.disable_warnings()
+cwd = os.getcwd()
 
-print("mapGen - orthophotomaps and terrain shading maps downloading for a specific area")
+if locale.getdefaultlocale()[0] == 'pl_PL':
+    lang = ['mapGen - program pobierający zdjęcia lotnicze (ortofotomapy) i mapy cieniowania terenu dla określonego obszaru\n', 'Nie można wczytać pliku konfiguracyjnego - wprowadź dane ręcznie.', 'Wczytywanie pliku konfiguracyjnego...', '\nKraj: %s, wymiary mapy: %s', 'Kraj (PL/CZ/SK): ', 'Współrzędne granic mapy [w stopniach dziesiętnych]:\n- Północ: ', '- Południe: ', '- Zachód: ', '- Wschód:', 'Wymiary mapy [m]: ',
+            '\nWybierz wersję FS, dla której mają być przygotowane tekstury:\n1. FS 22\n2. FS 19 i starsze\n(1/2): ', '\nUwaga: wymiary mapy muszą być równe 2^n', '\nPobieranie danych...', '\n\nKonwersja tekstur terenu do formatu DDS...', '\nZapisywanie PDA i mapy cieniowania terenu...', '\nWciśnij ENTER, aby zamknąć...']
+else:
+    lang = ['mapGen - orthophotomaps and terrain shading maps downloader for a specific area\n', "Can't load the config file - you must enter your data manually.", 'Config file is loading...', '\nCountry: %s, Map dimensions: %s', 'Country (PL/CZ/SK): ', "Map bounds' coordinates [decimal degrees]:\n- North: ", '- South: ', '- West: ', '- East: ', 'Map dimensions [meters]: ',
+            '\nChoose the FS version for which textures will be prepared:\n1. FS 22\n2. FS 19 and older\n(1/2): ', '\nWarning: map dimensions must be equal 2^n', '\nDownloading data...', '\n\nConverting terrain textures to DDS format...', '\nSaving map overview image and terrain shading map...', '\nPress ENTER to close...']
+
+print(lang[0])
 use_stdin = True
 try:
     config = open("demGenerator_config.txt", "r")
 except:
-    print("Can't load the config file - you must enter your data manually.")
+    print(lang[1])
 else:
-    print("Config file is loading...")
+    print(lang[2])
     string = config.read()
     string = string.split()
-    country = string[0]
+    country = string[0].upper()
     n = float(string[1])
     s = float(string[2])
     w = float(string[3])
     e = float(string[4])
     l = int(string[5])
-    print("Country: %s, Map's edge length: %s" % (country, l))
+    print(lang[3] % (country, l))
     use_stdin = False
     config.close()
 
 if use_stdin:
-    country = input("Country code (PL/CZ/SK): ")
-    n = float(input("Map bounds' coordinates [decimal degrees]:\n- North: "))
-    s = float(input("- South: "))
-    w = float(input("- West: "))
-    e = float(input("- East: "))
-    l = int(input("Map's edge length: "))
+    country = input(lang[4]).upper()
+    n = float(input(lang[5]))
+    s = float(input(lang[6]))
+    w = float(input(lang[7]))
+    e = float(input(lang[8]))
+    l = int(input(lang[9]))
 
 if s > n:
     n, s = s, n
@@ -37,36 +47,44 @@ if w > e:
     w, e = e, w
 
 while country != "CZ" and country != "PL" and country != "SK":
-    country = input("Re-enter the country code (PL/CZ/SK): ")
+    country = input(lang[4]).upper()
 
-if country == 'SK':
-    import requests
-    requests.urllib3.disable_warnings()
-else:
-    import urllib.request
-
-if country == 'CZ' or country == 'SK':
-    from PIL import Image
+fs = 0
+while fs != 1 and fs != 2:
+    fs = int(input(lang[10]))
 
 if math.log2(l) != int(math.log2(l)):
-    print("Error: map's edge length must be equal 2^n")
+    print(lang[11])
 else:
-    print("Downloading data...")
+    print(lang[12])
+
+    if (not os.path.exists(cwd + "\\demGen_data\\textures")):
+        os.mkdir(cwd + "\\demGen_data\\textures")
+
+    shadmap = Image.new('RGB', (l, l))
+    
     l = int(l / 1024)
+
+    if l * 2048 <= 16384:
+        pda_im = Image.new('RGB', (l * 2048, l * 2048))
+    else:
+        pda_im = Image.new('RGB', (16384, 16384))
+    
     if (l < 1):
         l = 1
     ortos = []
-    topos = []
+    # topos = [] (unused var)
     shads = []
     dx = (n - s) / l
     dy = (e - w) / l
+    pbar = tqdm.tqdm(total = l ** 2)
     for i in range(l):
         for j in range(l):
             x1 = i * dx + s
             x2 = (i + 1) * dx + s
             y1 = j * dy + w
             y2 = (j + 1) * dy + w
-            if country == "CZ":
+            if country == 'CZ':
                 url = "https://geoportal.cuzk.cz/WMS_ORTOFOTO_PUB/service.svc/get?LAYERS=GR_ORTFOTORGB&TRANSPARENT=FALSE&FORMAT=image%2Fpng&VERSION=1.3.0&EXCEPTIONS=XML&SERVICE=WMS&REQUEST=GetMap&STYLES=&CRS=EPSG%3A4326&_OLSALT=0.031478753454327824&BBOX=" + str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2) + "&WIDTH=2000&HEIGHT=2000"
             elif country == 'SK':
                 url = 'https://zbgis.skgeodesy.sk/zbgis/rest/services/Ortofoto/MapServer/export?dpi=96&transparent=true&format=png32&layers=show%3A5%2C6&bbox=' + str(y1) + "," + str(x1) + "," + str(y2) + "," + str(x2) + '&bboxSR=4326&imageSR=102100&size=2043%2C2048&f=image'
@@ -76,47 +94,76 @@ else:
             connerr = True
             while connerr:
                 try:
-                    if country == 'SK':
-                        orto = requests.get(url)
-                    else:
-                        orto = urllib.request.urlopen(url)
+                    orto = requests.get(url)
                 except:
                     continue
                 else:
-                    connerr = False
-                    fn = "demGen_data/orto_%d.png" % (i * l + j)
-                    if country == 'SK':
-                        open(fn, "wb").write(orto.content)
-                    else:
-                        open(fn, "wb").write(orto.read())
-                    orto.close()
-                    if country == 'CZ' or country =='SK':
-                        im = Image.open(fn)
-                        im.resize((2048, 2048), resample = Image.NEAREST).save(fn, 'PNG')
+                    if orto.status_code == 200:
+                        connerr = False
+                        im = Image.open(io.BytesIO(orto.content))
+                        if country != 'PL':
+                            im = im.resize((2048, 2048), Image.NEAREST)
+                        if l * 2048 <= 16384:
+                            pda_im.paste(im, (j * 2048, (l - i - 1) * 2048))
+                        else:
+                            pda_im.paste(im.resize((16384 / l, 16384 / l), Image.LANCZOS), (j * 16384 / l, (l - i - 1) * 16384 / l))
+                        if fs == 1:
+                            for k in range(2):
+                                for m in range(2):
+                                    nr = 2 * (l * (2 * i + k) + j) + m + 1
+                                    if nr < 10:
+                                        nr = '0%d' % nr
+                                    else:
+                                        nr = str(nr)
+                                    im.crop((m * 1024, (1 - k) * 1024, (m + 1) * 1024, (2 - k) * 1024)).transpose(Image.FLIP_TOP_BOTTOM).save('demGen_data/textures/ortho%s_diffuse.png' % nr, 'PNG')
+                        else:
+                            nr = i * l + j + 1
+                            if nr < 10:
+                                nr = '0%d' % nr
+                            else:
+                                nr = str(nr)
+                            im.transpose(Image.FLIP_TOP_BOTTOM).save('demGen_data/textures/ortho%s_diffuse.png' % nr, 'PNG')
                         im.close()
-            if (country != 'SK'):
+                    del orto
+
+            if country != 'SK':
                 if country == "CZ":
-                    url = "https://ags.cuzk.cz/arcgis2/services/dmr5g/ImageServer/WMSServer?LAYERS=dmr5g%3AGrayscaleHillshade&TRANSPARENT=FALSE&FORMAT=image%2Fpng&VERSION=1.3.0&EXCEPTIONS=INIMAGE&SERVICE=WMS&REQUEST=GetMap&STYLES=&CRS=EPSG%3A4326&_OLSALT=0.4763809632942847&BBOX=" + str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2) + "&WIDTH=4096&HEIGHT=4096"
+                    url = "https://ags.cuzk.cz/arcgis2/services/dmr5g/ImageServer/WMSServer?LAYERS=dmr5g%3AGrayscaleHillshade&TRANSPARENT=FALSE&FORMAT=image%2Fpng&VERSION=1.3.0&EXCEPTIONS=INIMAGE&SERVICE=WMS&REQUEST=GetMap&STYLES=&CRS=EPSG%3A4326&_OLSALT=0.4763809632942847&BBOX=" + str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2) + "&WIDTH=2048&HEIGHT=2048"
                 else:
-                    url = "http://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/GRID1/WMS/ShadedRelief?&REQUEST=GetMap&TRANSPARENT=FALSE&FORMAT=image/png&VERSION=1.3.0&LAYERS=Raster&STYLES=&BBOX=" + str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2) + "&CRS=EPSG:4326&EXCEPTIONS=xml&WIDTH=4096&HEIGHT=4096"
+                    url = "http://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/GRID1/WMS/ShadedRelief?&REQUEST=GetMap&TRANSPARENT=FALSE&FORMAT=image/png&VERSION=1.3.0&LAYERS=Raster&STYLES=&BBOX=" + str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2) + "&CRS=EPSG:4326&EXCEPTIONS=xml&WIDTH=2048&HEIGHT=2048"
                 shads.append(url)
                 connerr = True
                 while connerr:
                     try:
-                        shad = urllib.request.urlopen(url)
+                        shad = requests.get(url)
                     except:
                         continue
                     else:
-                        connerr = False
-                        open("demGen_data/shad_%d.png" % (i * l + j), "wb").write(shad.read())
-                        shad.close()
+                        if shad.status_code == 200:
+                            connerr = False
+                            im = Image.open(io.BytesIO(shad.content))
+                            shadmap.paste(im.resize((1024, 1024), Image.LANCZOS), (j * 1024, (l - i - 1) * 1024))
+                            im.close()
+                        del shad
+            pbar.update(1)
+            
+    print(lang[13])
+    if fs == 1:
+        subprocess.run(cwd + '\\textureTool -inputs "demGen_data\\textures" -arch dds -format bc7 -numMipmaps 8', stdout = subprocess.DEVNULL)
+    else:
+        subprocess.run(cwd + '\\texconv.exe -r -f DXT5 -l -o "demGen_data\\textures" "demGen_data\\textures\\*.png"', stdout = subprocess.DEVNULL)
+
+    print(lang[14])
+    pda_im.save('demGen_data/mapOverwiev.png', 'PNG')
+    shadmap.save('demGen_data/terrainShading.png', 'PNG')
+
     urllist = open('demGen_data/mapGenURLs.txt', 'w')
-    urllist.write('Orthophotomaps:\n')
+    urllist.write('Ortho images:\n')
     for i in range(len(ortos)):
         urllist.write(ortos[i] + '\n')
     if country != 'SK':
         urllist.write('\nTerrain shading maps:\n')
         for i in range(len(shads)):
-        	urllist.write(shads[i] + '\n')
+            urllist.write(shads[i] + '\n')
     urllist.close()
-w = input("Press ENTER to close...")
+w = input(lang[15])
--- requirementsChecker.py
+++ requirementsChecker.py
@@ -1,21 +1,21 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import locale
-import platform
-import os
+import locale, platform, os, shutil
 
 if locale.getdefaultlocale()[0] == 'pl_PL':
-    lang = ('Program sprawdzający wymagania demGeneratora\n\nSprawdzanie wersji środowiska Pythona...', 'Wersja: %s.%s.%s - poprawna', '\nPróba zaimportowania modułu "%s"...', 'Nie znaleziono tego modułu - teraz nastąpi jego instalacja.\n--------------------', 'Moduł pomyślnie zaimportowano', 'demGen nie wspiera Pythona w wersji %s.%s.%s. Uruchom go w 3.5 - 3.7.', '\nWciśnij ENTER, aby zamknąć...')
+    lang = ('Program sprawdzający wymagania demGeneratora\n\nSprawdzanie wersji środowiska Pythona...', 'Wersja: %s.%s.%s - poprawna', '\nPróba zaimportowania biblioteki "%s"...', '\nNie znaleziono tej biblioteki - teraz nastąpi jego instalacja.\n--------------------', ' SUKCES', '\nSprawdzanie obecności programu Texconv...', '\nNie znaleziono – trwa pobieranie...',
+            '\nSprawdzanie obecności programu GIANTS Texture Tool...', '\nSkopiowano do głównego folderu demGeneratora', '\nNie znaleziono - pobierz ZIPa z programem z GIANTS Developer Network, wypakuj go i spróbuj ponownie.', 'demGen nie wspiera Pythona w wersji %s.%s.%s. Uruchom go w 3.5 - 3.7.', '\nWciśnij ENTER, aby zamknąć...')
 else:
-    lang = ('demGen requirements checker\n\nPython version checking...', 'Version: %s.%s.%s - correct', '\nTrying to import the "%s" module...', 'Module not found - it will be installed now.\n--------------------', 'Module imported successfully', 'demGen has no support for Python %s.%s.%s. Run it in the Python 3.5 - 3.7 environment.', '\nPress ENTER to close...')
+    lang = ('demGen requirements checker\n\nPython version checking...', 'Version: %s.%s.%s - correct', '\nTrying to import the "%s" library...', 'Library not found - it will be installed now.\n--------------------', ' SUCCESS', '\nChecking the Texconv program presency...', '\nNot found – downloading in progress...',
+            '\nChecking the GIANTS Texture Tool presency...', "\nCopied to demGen's main folder", '\nNot found - download ZIP with this program from GIANTS Developer Network, unzip it and retry.', 'demGen has no support for Python %s.%s.%s. Run it in the Python 3.5 - 3.7 environment.', '\nPress ENTER to close...')
 
 print(lang[0])
 ver = platform.python_version_tuple()
 if ver[0] == '3' and ver[1] >= '5' and ver[1] <= '7':
     print(lang[1] % (ver[0], ver[1], ver[2]))
     
-    print(lang[2] % 'Pillow')
+    print(lang[2] % 'Pillow', end = '')
     success = False
     while not success:
         try:
@@ -28,7 +28,7 @@ if ver[0] == '3' and ver[1] >= '5' and ver[1] <= '7':
             print(lang[4])
             success = True
 
-    print(lang[2] % 'tqdm')
+    print(lang[2] % 'tqdm', end = '')
     success = False
     while not success:
         try:
@@ -40,7 +40,7 @@ if ver[0] == '3' and ver[1] >= '5' and ver[1] <= '7':
         else:
             print(lang[4])
             success = True
-    print(lang[2] % 'requests')
+    print(lang[2] % 'requests', end = '')
     success = False
     while not success:
         try:
@@ -52,27 +52,45 @@ if ver[0] == '3' and ver[1] >= '5' and ver[1] <= '7':
         else:
             print(lang[4])
             success = True
-    """ not now
+    print(lang[5], end = '')
     if os.path.exists(os.getcwd() + '\\texconv.exe'):
-        print(lang[6])
+        print(' OK')
     else:
-        print(lang[5])
-        import urllib.request as req
+        print(lang[6])
+        import requests
         success = False
         while not success:
             try:
-                f = req.urlopen('https://github.com/Microsoft/DirectXTex/releases/latest/download/texconv.exe')
+                f = requests.get('https://github.com/Microsoft/DirectXTex/releases/latest/download/texconv.exe')
             except:
                 continue
             else:
-                if f.getcode() == 200:
-                    open('texconv.exe', 'wb').write(f.read())
-                    print(lang[8])
+                if f.status_code == 200:
+                    open('texconv.exe', 'wb').write(f.content)
+                    print(lang[4])
                     success = True
-    """
+    print(lang[7], end = '')
+    if os.path.exists(os.getcwd() + '\\textureTool.exe') and os.path.exists(os.getcwd() + '\\textureTool.xml'):
+        print(' OK')
+    else:
+        root_dir = ''
+        if os.path.exists(os.getcwd() + '\\textureTool.exe') and not os.path.exists(os.getcwd() + '\\textureTool.xml'):
+            for root, dirs, files in os.walk('C:\\'):
+                if 'textureTool.xml' in files:
+                    root_dir = root
+        else:
+            for root, dirs, files in os.walk('C:\\'):
+                if 'textureTool.exe' in files:
+                    root_dir = root
+        if root_dir == '':
+            print(lang[9])
+        else:
+            shutil.copy(root_dir + '\\textureTool.exe', os.getcwd())
+            shutil.copy(root_dir + '\\textureTool.xml', os.getcwd())
+            print(lang[8])
 else:
-    print(lang[5] % (ver[0], ver[1], ver[2]))
+    print(lang[10] % (ver[0], ver[1], ver[2]))
     import webbrowser
     webbrowser.open('www.python.org/downloads/release/python-377/')
 
-_ = input(lang[6])
+_ = input(lang[11])
